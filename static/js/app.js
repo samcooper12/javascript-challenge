e0d52710f2cd939fa87fa0f95706bbae89fabc91
Sam Cooper <samcooperpdx@gmail.com>
2019-12-22 15:09:38 -0800

    final

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,21 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var filter = '';
+var dateFilter; 
+
+var submit = d3.select('#filter-btn')
+
+var res = [];
 
 var tbody = d3.select('tbody')
 
+function clearTable(){
+  var toRemove = tbody.selectAll('tr')
+  toRemove.remove()
+}
+
+// init 
 
 data.forEach((siting) => {
   var row = tbody.append("tr");
@@ -16,26 +27,82 @@ data.forEach((siting) => {
 
 console.log(data)
 
-
-var submit = d3.select('#filter-btn')
-
-res = [];
-
 submit.on("click", function() {
-  var filter = d3.select('#datetime').node()
-  var dateFilter = filter.value
 
+  filter = d3.select('#datetime').node()
+  dateFilter = filter.value
 
-  var filteredResults = data.filter(function(data) {
-  	
-  	data.forEach((siting) => {
+  // if (dateFilter !==  '') {
 
-  		if(siting.datetime = dateFilter) {
-  		res.append(siting) }
-  		})
+    clearTable();
+  // }
+
+  // else {
+  //   return
+  // }
+  // data.forEach((siting) => { //   if() { //   } //  console.log(siting.datetime) //   }) // })
+
+  data.forEach((siting) => {
+    var check = siting.datetime
+    console.log('check   ', check)
+    console.log('~~~~', dateFilter, '~~~~')
+
+  //  var row = tbody.append("tr");
+    if (dateFilter == check){
+
+      var row = tbody.append("tr")
+                Object.entries(siting).forEach(([key, value]) => {
+
+                  var cell = row.append("td");
+                  cell.text(value);
+                })
+    }
+    else {
+      return
+    }
+
+    // else {
+    //   console.log("return in else is next");
+    //   return
+    // }
+  // var row = tbody.append("tr");
+  // Object.entries(siting).forEach(([key, value]) => {
+  //   var cell = row.append("td");
+  //   cell.text(value);
   })
-  // Set the counter h1 text to the new count
-});
+  });
+//});
+
+//   var filter = d3.select('#datetime').node()
+//   console.log('function ran - var filter ', filter)
+//   var dateFilter = filter.value
+// })
+//   function queryInput(input) {
+
+//     if (true) {
+
+//     }
+//   }
+
+// }
+  // console.log("event")
+  // var filter = d3.select('#datetime').node()
+  // console.log('function ran - var filter ', filter)
+  // var dateFilter = filter.value
+  // console.log(dateFilter)
+//   console.log('function ran - var dateFilter (value of filter)  ', dateFilter )
+// });
+
+          //   var filteredResults = data.filter(function(data) {
+            	
+          //   	data.forEach((siting) => {
+
+          //   		if(siting.datetime = dateFilter) {
+          //   		res.append(siting) }
+          //   		})
+          //   })
+          //   // Set the counter h1 text to the new count
+          // });
 
 
 // var filtered = roster.filter(function(
